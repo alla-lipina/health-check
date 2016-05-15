f55320b9980cd983e362f16170adf1193f654fd4
Alla Lipina <alla.lipina@gmail.com>
2016-05-15 13:50:31 +0200

    Use haml, add organization controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,63 @@
+class OrganizationsController < ApplicationController
+  before_action :set_organization, only: [:show, :edit, :update, :destroy]
+
+  # GET /organizations
+  # GET /organizations.json
+  def index
+    @organizations = Organization.all
+  end
+
+  # GET /organizations/1
+  # GET /organizations/1.json
+  def show
+  end
+
+  # GET /organizations/new
+  def new
+    @organization = Organization.new
+  end
+
+  # GET /organizations/1/edit
+  def edit
+  end
+
+  # POST /organizations
+  # POST /organizations.json
+  def create
+    @organization = Organization.new(organization_params)
+
+    if @organization.save
+      redirect_to @organization, notice: 'Organization was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /organizations/1
+  # PATCH/PUT /organizations/1.json
+  def update
+    if @organization.update(organization_params)
+      redirect_to @organization, notice: 'Organization was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /organizations/1
+  # DELETE /organizations/1.json
+  def destroy
+    @organization.destroy
+    redirect_to organizations_url, notice: 'Organization was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def organization_params
+      params.require(:organization).permit(:name)
+    end
+end
