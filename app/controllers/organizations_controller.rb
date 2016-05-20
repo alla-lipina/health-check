2745420c51f426c878a9cfdc811d527e310ddbbd
Alla Lipina <alla.lipina@gmail.com>
2016-05-20 16:01:34 +0200

    Adjust question controller and views

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,28 +1,20 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
 
-  # GET /organizations
-  # GET /organizations.json
   def index
     @organizations = Organization.all
   end
 
-  # GET /organizations/1
-  # GET /organizations/1.json
   def show
   end
 
-  # GET /organizations/new
   def new
     @organization = Organization.new
   end
 
-  # GET /organizations/1/edit
   def edit
   end
 
-  # POST /organizations
-  # POST /organizations.json
   def create
     @organization = Organization.new(organization_params)
 
@@ -33,8 +25,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /organizations/1
-  # PATCH/PUT /organizations/1.json
   def update
     if @organization.update(organization_params)
       redirect_to @organization, notice: 'Organization was successfully updated.'
@@ -43,8 +33,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # DELETE /organizations/1
-  # DELETE /organizations/1.json
   def destroy
     @organization.destroy
     redirect_to organizations_url, notice: 'Organization was successfully destroyed.'
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,48 +1,54 @@
 class QuestionsController < ApplicationController
+  before_action :set_organization
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @questions = Question.all
+    @questions = @organization.questions.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
-    @question = Question.new
+    @question = @organization.questions.new
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = @organization.questions.new(question_params)
     
     if @question.save
-      redirect_to @question
+      redirect_to organization_question_url
     else
       render 'new'
     end
   end
 
   def update
-    @question = Question.find(params[:id])
-   
     if @question.update(question_params)
-      redirect_to @question
+      redirect_to organization_question_url
     else
       render 'edit'
     end
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
    
     redirect_to questions_path
   end
 
   private
+    def set_question
+      @question = @organization.questions.find(params[:id])
+    end
+
+    def set_organization
+      @organization = Organization.find(params[:organization_id])
+    end
+
     def question_params
       params.require(:question).permit(:title, :text)
     end
