cbb5cf2155d1cbff95b92315248c1c8935b13e8e
Alla Lipina <alla.lipina@gmail.com>
2016-05-20 17:08:49 +0200

    Add survey controller and views

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,61 @@
+class SurveysController < ApplicationController
+  before_action :set_survey, only: [:show, :edit, :update, :destroy]
+  before_action :set_organization, only: [:show]
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    #TODO: show the results of the survey only if stop_at passed
+    if @survey.stop_at.past?
+      render :show
+    else
+      redirect_to surveys_url, notice: 'The survey is not finished yet.'
+    end
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def edit
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      redirect_to @survey, notice: 'Survey was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # organization can prolong the survey
+  def update
+    if @survey.update(survey_params)
+      redirect_to @survey, notice: 'Survey was successfully updated.'
+    else
+      render :edit 
+    end
+  end
+
+  def destroy
+    @survey.destroy
+    redirect_to surveys_url, notice: 'Survey was successfully destroyed.'
+  end
+
+  private
+    def set_survey
+      @survey = Survey.find(params[:id])
+    end
+
+    def set_organization
+      @organization = Organization.first
+    end
+
+    def survey_params
+      params.require(:survey).permit([:start_at, :stop_at])
+    end
+end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,11 @@ class Vote < ActiveRecord::Base
   belongs_to :question
   belongs_to :team
   belongs_to :survey
+
+  scope :by_team, -> (id) { where(team_id: id) }
+  scope :by_survey, -> (id) { where(survey_id: id) }
+
+  def self.avg
+    average(:value)
+  end
 end
