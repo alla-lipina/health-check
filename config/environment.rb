14881df9ff3c52dd8b0d95746133158a16fbbead
Alla Lipina <alla.lipina@gmail.com>
2016-07-07 11:57:06 +0300

    Add settings for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'alla.lipina ',
   :password => '1200MicS',
-  :domain => 'yourdomain.com',
+  :domain => '95.85.8.62',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
